21ecc46a3cb4730e83e007ba8fa243df9215c29f
derder0041 <17634381172@163.com>
2024-05-01 14:14:23 +0800

    Site updated: 2024-05-01 14:14:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/30/hello-world/"];function toRandomPost(){
+var posts=["2024/05/01/test/","2024/04/30/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
