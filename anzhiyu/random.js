63786ffe0ac67fa478e35731a85d53d07826fc40
derder0041 <3481722507@qq.com>
2024-05-01 23:15:15 +0800

    Site updated: 2024-05-01 23:15:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/30/hello-world/","2024/05/01/test/"];function toRandomPost(){
+var posts=["2024/04/30/hello-world/","2024/05/01/test/","2024/05/01/mc联机教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
